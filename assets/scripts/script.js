94f1f5405a63f8af986d162fc97cb455e35c30a5
Moses Mcglynn <145668520+mosesmcglynn@users.noreply.github.com>
2023-12-13 01:15:38 +0800

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,3 +23,51 @@ cards.forEach(function(card) {
         p.classList.remove("open");
     });
 });
+
+// gallery
+
+var gallery = document.querySelectorAll('.gallery .galleryimg'),
+    galleryslider = document.querySelector('.gallery-slider'),
+    images = document.querySelectorAll('.gallery-slider .galleryimg'),
+    prevbtn = document.querySelector('.nav-button.prev'),
+    nextbtn = document.querySelector('.nav-button.next'),
+    exitbtn = document.querySelector('.exit');
+
+var currentIndex = 0;
+
+function updateButtons(currentIndex) {
+    prevbtn.style.visibility = currentIndex > 0 ? 'visible' : 'hidden';
+    nextbtn.style.visibility = currentIndex < images.length - 1 ? 'visible' : 'hidden';
+}
+
+gallery.forEach(function(img, index) {
+    img.addEventListener('click', function() {
+        galleryslider.classList.toggle('open');
+        images[index].classList.add('active');
+        currentIndex = index;
+        updateButtons(currentIndex);
+    });
+});
+    
+images[currentIndex].classList.add('active');
+
+prevbtn.addEventListener('click', function() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateButtons(currentIndex);
+    images[currentIndex].classList.add('active');
+});
+
+nextbtn.addEventListener('click', function() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    updateButtons(currentIndex);
+    images[currentIndex].classList.add('active');
+});
+
+exitbtn.addEventListener('click', function() {
+    document.querySelector('.gallery-slider').classList.toggle('open');
+    images[currentIndex].classList.remove('active');
+});
+
+updateButtons(currentIndex);
